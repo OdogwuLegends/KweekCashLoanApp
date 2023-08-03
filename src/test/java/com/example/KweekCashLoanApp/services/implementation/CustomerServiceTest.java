ced20604a78aab138621c7cf282267dbe4e0777d
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-03 16:46:44 +0100

    more changes made

--- src/test/java/com/example/KweekCashLoanApp/services/implementation/CustomerServiceTest.java
+++ src/test/java/com/example/KweekCashLoanApp/services/implementation/CustomerServiceTest.java
@@ -1,5 +1,9 @@
 package com.example.KweekCashLoanApp.services.implementation;
 
+import com.example.KweekCashLoanApp.dtos.requests.RegisterUserRequest;
+import com.example.KweekCashLoanApp.dtos.responses.RegisterUserResponse;
+import com.example.KweekCashLoanApp.error.IncorrectDetailsException;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,6 +14,33 @@ class CustomerServiceTest {
     private CustomerService customerService;
 
 
+    @Test
+    void testCustomerServiceNotNull(){
+        assertNotNull(customerService);
+    }
+    @Test
+    void testToRegisterCustomerWithCorrectDetails(){
+        RegisterUserRequest request = new RegisterUserRequest();
+
+        request.setFirstName("Favour");
+        request.setLastName("Black");
+        request.setEmail("fav93@gmail.com");
+        request.setPassword("12BigBlackFav");
+        request.setOccupation("Banking");
+        request.setPhoneNumber("08045983697");
+        request.setStreetNumber("34");
+        request.setStreetName("Curtis Jones Street");
+        request.setTown("Ikeja");
+        request.setState("Lagos");
+
+        RegisterUserResponse savedCustomer = new RegisterUserResponse();
+        try {
+            savedCustomer = customerService.registerCustomer(request);
+        } catch (IncorrectDetailsException e) {
+            System.err.println(e.getMessage());;
+        }
+        assertEquals("Welcome "+savedCustomer.getFirstName() + " "+savedCustomer.getLastName()+". Your account creation was successful.",savedCustomer.toString());
+    }
 
 
 }
